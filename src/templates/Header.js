4df37ef7c519a553b1068ff560d5f14b89e62a3e
DCoreas <josuepaiz487@gmail.com>
2021-05-14 23:03:40 -0600

    Fix main route again 2

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
       <div class="Header-main">
         <div class="Header-logo">
           <h1>
-            <a href="/spa-js/">100tifi.co</a>
+            <a href="/spa-js/">100tifi.com</a>
           </h1>
         </div>
         <div class="Header-nav">
